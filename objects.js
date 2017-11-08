43bdf882f98293cb395b6380613ff0523aa31975
Sean Derrow <sderrow@mit.edu>
2017-11-08 04:05:48 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {beatles: "Hey Jude"}
+
+function updatePlaylist(my_playlist, artist, song) {
+  return Object.assign(my_playlist, {artist: song})
+}
+
+function removeFromPlaylist(my_playlist, artist) {
+  delete my_playlist.artist
+  return my_playlist
+}
